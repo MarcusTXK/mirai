1c4a06dfa43633284c320c73947ef8e0ebb8003f
Marcus Tang <txk.marcus@gmail.com>
2023-09-18 01:19:36 +0800

    Add communication to llm

---
+++ README.md
@@ -0,0 +1,9 @@
+### Attempting to get ESP32 to communicate with a local LLM
+
+Windows:
+
+```
+py -m venv venv
+venv/Scripts/activate.ba
+pip install -r requirements.txt
+```
--- test.py
+++ test.py
@@ -1,52 +1,77 @@
-from langchain.prompts import (
-    ChatPromptTemplate,
-    MessagesPlaceholder,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
+from langchain.llms import LlamaCpp
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 import paho.mqtt.client as mqtt
 from dotenv import load_dotenv
 import json
+from "langchain/output_parsers" import { StructuredOutputParser } 
 
-MQTT_SERVER = "broker.mqrrdashboard.com"
-CLIENTID = "solomon_client_00198"
+MQTT_SERVER = "broker.hivemq.com"
+CLIENTID = "esp32-dht22-clientId-cdf7"
 PASSWORD = ""
 SUBTOPIC_LED = "esp32-dht22/LED"
 SUBTOPIC_DOOR = "esp32-dht22/DOOR"
+SUBTOPIC_TEMP = "esp32-dht22/Temp"
+SUBTOPIC_HUMIDITY = "esp32-dht22/Humidity"
+MODEL = r"C:\Users\user\Documents\Programming\NUS\FYP\esp32-llm-bridge\models\llama-2-7b.Q4_K_M.gguf"
+
+
+def on_message(client, userdata, msg):
+    topic = msg.topic
+    payload = msg.payload.decode("utf-8")
+
+    print(f"Received message from topic {topic}: {payload}")
 
 
 client = mqtt.Client()
 client.username_pw_set(CLIENTID)
-llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-k = 0  # keep last k interactions in memory
-memory = ConversationBufferWindowMemory(k=k, return_messages=True)
+client.connect(MQTT_SERVER, 1883, 60)
+client.on_message = on_message
+# Subscribe to the topics for temperature and humidity
+client.subscribe(SUBTOPIC_TEMP)
+client.subscribe(SUBTOPIC_HUMIDITY)
 
 
-def create_chat(state):
-    system_message = f"""
-    
-    `no explanations`
-    `no prompt`
-    You are a wise steward, The current environment is represented in JSON:
+# llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+# k = 0  # keep last k interactions in memory
+# memory = ConversationBufferWindowMemory(k=k, return_messages=True)
+
+
+def send_chat(state, user_input):
+    prompt = f"""
+    The current environment is represented in JSON:
     {{{json.dumps(state)}}}
-    Please check the environment to update JSON.
-    Format your response as a JSON object with "msg", "light", "door" keys.
+    Please check the environment and update JSON.
+    1 represents on and 0 represents off
+    Format your response as a JSON object with "msg", "light", "door" keys
+    No explanations are required other than the json
+    {user_input}
     """
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            SystemMessagePromptTemplate.from_template(system_message),
-            MessagesPlaceholder(variable_name="history"),
-            HumanMessagePromptTemplate.from_template("{input}"),
-        ]
+
+
+    
+    # prompt = PromptTemplate(template=template, input_variables=["input"])
+    # Callbacks support token-wise streaming
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+    llm = LlamaCpp(
+        model_path=MODEL,
+        temperature=0.75,
+        max_tokens=2000,
+        top_p=1,
+        callback_manager=callback_manager,
+        verbose=True,  # Verbose is required to pass to the callback manager
     )
 
-    conversation = ConversationChain(
-        memory=memory, prompt=prompt, llm=llm, verbose=True
-    )
-    return conversation
+    # conversation = ConversationChain(
+    #     prompt=prompt, llm=llm, verbose=True  # memory=memory,
+    # )
+    return llm(prompt)
 
 
 def get_state(response):
@@ -59,6 +84,8 @@ def get_state(response):
 
 def main():
     load_dotenv()
+    # Start the loop to keep listening for incoming messages
+    client.loop_start()
 
     state = {"light": 0, "door": 0, "msg": "The light is off, the door is closed"}
 
@@ -68,14 +95,28 @@ def main():
         if len(user_input) == 0:
             continue
 
-        conversation = create_chat(state)
-        response = conversation.predict(input=user_input)
+        response = send_chat(state, user_input)
+        # response = conversation.predict(input=user_input)
         print(f"Assistant: {response}\n")
         try:
-            state = get_state(response)
+            state = get_state(state)
         except Exception as e:
             print(e)
 
 
 if __name__ == "__main__":
     main()
+
+
+# while True:
+#     print("Enter 'on' to turn on the light, 'off' to turn it off, or 'quit' to exit.")
+#     user_input = input("> ").strip().lower()
+
+#     if user_input == "quit":
+#         print("Exiting program.")
+#         break
+#     elif user_input in ["on", "off"]:
+#         client.publish(SUBTOPIC_LED, user_input)
+#         print(f"Sent '{user_input}' to topic {SUBTOPIC_LED}")
+#     else:
+#         print("Invalid input. Please enter 'on', 'off', or 'quit'.")
