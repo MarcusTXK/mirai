f0bec58abcd3813738ffa375b947f124dde06256
Marcus Tang <txk.marcus@gmail.com>
2023-10-03 01:39:43 +0800

    Fix unable to connect to MQTT functionality

--- blink/main/blink_example_main.c
+++ blink/main/blink_example_main.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
 #include "esp_log.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "nvs_flash.h"
 #include "mqtt_client.h"
-#include "esp_netif.h"
-#include "mqtt_client.h"
+#include "driver/gpio.h"
 
 static const char *TAG = "MQTT_APP";
-
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
 static uint8_t s_led_state = 1;
 
-static esp_mqtt_client_handle_t mqtt_client = NULL;
-void mqtt_event_handler_cb(void* handler_args, esp_event_base_t base, int32_t event_id, void* event_data) {
-    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t) event_data;
-    
+static void mqtt_event_handler(void* handler_args, esp_event_base_t base, int32_t event_id, void* event_data) {
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            esp_mqtt_client_subscribe(mqtt_client, "esp32-dht22/LED", 0);
+            esp_mqtt_client_subscribe(client, "esp32-dht22/LED", 0);
             break;
         case MQTT_EVENT_DATA:
             ESP_LOGI(TAG, "MQTT_EVENT_DATA");
@@ -40,10 +50,7 @@ void mqtt_event_handler_cb(void* handler_args, esp_event_base_t base, int32_t ev
     }
 }
 
-
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data)
-{
+static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
     } else if (event_id == WIFI_EVENT_STA_DISCONNECTED) {
@@ -53,57 +60,84 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
-void app_main(void)
+static void mqtt_app_start(void)
 {
-    // Initialize NVS (required for Wi-Fi configuration storage)
-    nvs_flash_init();
-
-    esp_netif_init();
-
-    esp_event_loop_create_default();
-
-    // Configure Wi-Fi
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-    esp_wifi_set_mode(WIFI_MODE_STA);
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = "Virus",
-            .password = "download",
-        },
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = CONFIG_BROKER_URL,
     };
+#if CONFIG_BROKER_URL_FROM_STDIN
+    char line[128];
 
-    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL);
+    if (strcmp(mqtt_cfg.broker.address.uri, "FROM_STDIN") == 0) {
+        int count = 0;
+        printf("Please enter url of mqtt broker\n");
+        while (count < 128) {
+            int c = fgetc(stdin);
+            if (c == '\n') {
+                line[count] = '\0';
+                break;
+            } else if (c > 0 && c < 127) {
+                line[count] = c;
+                ++count;
+            }
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
+        mqtt_cfg.broker.address.uri = line;
+        printf("Broker url: %s\n", line);
+    } else {
+        ESP_LOGE(TAG, "Configuration mismatch: wrong broker url");
+        abort();
+    }
+#endif /* CONFIG_BROKER_URL_FROM_STDIN */
 
-    esp_wifi_start();
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
+
+void app_main(void) {
+    // Initialization and Configuration
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+    esp_log_level_set("mqtt_example", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport", ESP_LOG_VERBOSE);
+    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // // Configure Wi-Fi
+    // wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    // esp_wifi_init(&cfg);
+    // esp_wifi_set_mode(WIFI_MODE_STA);
+    // wifi_config_t wifi_config = {
+    //     .sta = {
+    //         .ssid = "Virus",
+    //         .password = "download",
+    //     },
+    // };
+    // esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    // esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL);
+    // esp_wifi_start();
+    ESP_ERROR_CHECK(example_connect());
+
 
     // MQTT Configuration
-    // const esp_mqtt_client_config_t mqtt_cfg = {
-    //     .broker.address.uri = "mqtt://broker.hivemq.com",
-    //     // .broker.address.hostname = "broker.hivemq.com",
-    //     // .broker.address.port = 1883,
-    //     // .broker.address.transport = MQTT_TRANSPORT_OVER_TCP,
-    //     .credentials.username = "esp32-dht22-clientId-cdf7",
+    // esp_mqtt_client_config_t mqtt_cfg = {
+    //     .broker.address.uri = CONFIG_BROKER_URL,
     // };
-    const esp_mqtt_client_config_t mqtt_cfg = {
-        .broker = {
-            .address = {
-                .hostname = "broker.hivemq.com",
-                .port = 1883,
-                .transport = MQTT_TRANSPORT_OVER_TCP,
-            }
-        },
-        .credentials = {
-            .username = "esp32-dht22-clientId-cdf7",
-        },
-    };
-
-
-    mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_register_event(mqtt_client, MQTT_EVENT_ANY, mqtt_event_handler_cb, mqtt_client);
-    esp_mqtt_client_start(mqtt_client);
+    // esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    // esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    // esp_mqtt_client_start(client);
+    mqtt_app_start();
 
 
     // Configure the LED
